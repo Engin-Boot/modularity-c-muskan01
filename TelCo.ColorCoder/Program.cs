f6344a3e4b665a47be205423d0239d4bfb480d1a
muskan01 <35635037+muskan01@users.noreply.github.com>
2020-09-05 19:10:41 +0530

    Update Program.cs

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -3,8 +3,7 @@ using System.Diagnostics;
 using System.Drawing;
 
 namespace TelCo.ColorCoder
-{
-    /// <summary>
+{   /// <summary>
     /// The 25-pair color code, originally known as even-count color code, 
     /// is a color code used to identify individual conductors in twisted-pair 
     /// wiring for telecommunications.
@@ -13,35 +12,6 @@ namespace TelCo.ColorCoder
     /// </summary>
   partial class Program
     {
-        /// <summary>
-        /// Array of Major colors
-        /// </summary>
-        private static Color[] colorMapMajor;
-        /// <summary>
-        /// Array of minor colors
-        /// </summary>
-        private static Color[] colorMapMinor;
-        /// <summary>
-        /// data type defined to hold the two colors of clor pair
-        /// </summary>
-        internal class ColorPair
-        {
-            internal Color majorColor;
-            internal Color minorColor;
-            public override string ToString()
-            {
-                return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
-            }
-        }
-        /// <summary>
-        /// Static constructor required to initialize static variable
-        /// </summary>
-        static Program()
-        {
-            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
-            colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
-        }
-
         /// <summary>
         /// Test code for the class
         /// </summary>
