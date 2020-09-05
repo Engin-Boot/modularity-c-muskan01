f3a42d3688ba557be91274e0a859526f5ca8eb2e
muskan01 <35635037+muskan01@users.noreply.github.com>
2020-09-05 20:58:54 +0530

    Create ToString.cs

---
+++ TelCo.ColorCoder/ToString.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace TelCo.ColorCoder
+{ 
+   partial class Program
+    { 
+     public static void ToString()
+     {      int PairNumber = 0;
+            string Manual = "";
+            string PairNumberInString = "";
+ 
+          for(int MajorColorno= 0; MajorColorno < colorMapMajor.Length ; MajorColorno++)
+          {
+            for(int MinorColorno= 0; MinorColorno < colorMapMinor.Length ; MinorColorno++)
+              {   
+              string majorcolorName = colorMapMajor[MajorColorno];
+              string minorcolorName = colorMapMinor[MinorColorno];      
+              PairNumber = PairNumber + 1;
+              PairNumberInString = PairNumber .ToString();
+              Manual = Manual + PairNumberInString + ". " + "Major: " + majorcolorName + ", Minor: " + minorcolorName + "\n";
+              }
+          }
+ Console.WriteLine(Manual);
+        } 
+     }
+}
